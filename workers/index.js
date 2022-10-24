e3701f38e001939cc64af9715eb83641909f71ed
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-10-23 22:47:15 -0400

    added input validation

--- workers/index.js
+++ workers/index.js
@@ -22,12 +22,15 @@ const invalid = `<DOCTYPE html>
  */
 async function handleRequest(request) {
   const { searchParams } = new URL(request.url);
+  const regex = new RegExp('(https:\/\/)?gist.github.com\/[a-zA-Z0-9_\-]*\/([a-zA-Z0-9]*)');
 
-  let gist_url =  searchParams.get('gist_url') ?? null
+  let gist_url = searchParams.get('gist_url') ?? null;
+  let assert_url = regex.test(gist_url);
+
 
   console.log(gist_url)
 
-  if (gist_url == null || gist_url == '') {
+  if (gist_url == null || gist_url == '' || assert_url == false) {
     return new Response(invalid, {
       headers: {
         'content-type': 'text/html;charset=UTF-8'
